89a2862aa2b5ae82da715845986cb2255d913a19
woojooheuk <dnheuk@Naver.com>
2024-05-21 18:57:51 +0900

    내 씬 UI 바꾼거

--- Assets/saveOurGradu/LastClickedLightObj.cs
+++ Assets/saveOurGradu/LastClickedLightObj.cs
@@ -5,17 +5,14 @@ using UnityEngine;
 public class LastClickedLightObj : MonoBehaviour
 {
     private Light_Controller lightController;
-    private changeColor changecolor;
     // Start is called before the first frame update
     void Start()
     {
         lightController = GameObject.FindObjectOfType<Light_Controller>();
-        changecolor = GameObject.FindObjectOfType<changeColor>();
     }
 
     private void OnMouseDown()
     {
         lightController.SetLastClickedObject(gameObject);
-        changecolor.SetLastClickedObject(gameObject);
     }
 }
